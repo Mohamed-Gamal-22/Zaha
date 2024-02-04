3aa3ed444d7988eeac4851c34977f509f58b56b4
mohamed gamal <mogamall086@gmail.com>
2024-02-04 22:59:26 +0200

    add generate-react-cli

---
+++ Template/TemplateName.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./TemplateName.module.css";
+
+export default function Template() {
+  return (
+    <>
+      <h1>Template</h1>
+    </>
+  );
+}
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Home.module.css";
+
+export default function Template() {
+  return (
+    <>
+      <h1>Template</h1>
+    </>
+  );
+}
